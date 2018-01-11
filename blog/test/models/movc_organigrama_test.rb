c0d6bb449f48056844fa6c5d380dd3dbebb45499
fferreiro <fferreiro@uach.mx>
2018-01-11 08:52:36 -0700

    Elimino carpeta de blog

--- blog/test/models/movc_organigrama_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MovcOrganigramaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- blog/test/system/movd_organigrama_puestos_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class MovdOrganigramaPuestosTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit movd_organigrama_puestos_url
-  #
-  #   assert_selector "h1", text: "MovdOrganigramaPuesto"
-  # end
-end
